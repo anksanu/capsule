b1a9b67da3bb99b448a78cffb84f2d2005973087
Ankit Agrawal <agrawal.anksanu@gmail.com>
2014-01-25 10:58:21 +0530

    Refactored the code using JS Beautify

--- capsule.js
+++ capsule.js
@@ -1,26 +1,24 @@
-
 // Capsule Constructor
-capsule = function(){
-	var _storage
-	,	self = this;
+capsule = function() {
+    var _storage, self = this;
 
 
-	// Looks for the availability of global localStorage object in the browser
-	_storage = (function(){
-		try {
-			localStorage.setItem('foo', 'bar');
-			localStorage.getItem('foo');
-			localStorage.removeItem('foo');
-			return localStorage;
-		} catch(e) {
-			console.error('LocalStorage global Object not present');
-		}
-		return void 0;
-	})();
+    // Looks for the availability of global localStorage object in the browser
+    _storage = (function() {
+        try {
+            localStorage.setItem('foo', 'bar');
+            localStorage.getItem('foo');
+            localStorage.removeItem('foo');
+            return localStorage;
+        } catch (e) {
+            console.error('LocalStorage global Object not present');
+        }
+        return void 0;
+    })();
 
-	self._storage = _storage;
+    self._storage = _storage;
 
-	return _storage ? self : void 0;
+    return _storage ? self : void 0;
 };
 
 capsule.prototype = {};
@@ -28,29 +26,29 @@ capsule.prototype = {};
 
 // Capsule wrapper over localStorage native setItem method
 capsule.prototype.setItem = function(id, Obj) {
-	var self = this;
+    var self = this;
 
-	self._storage.setItem(id, Obj);
+    self._storage.setItem(id, Obj);
 
-	return Obj;
+    return Obj;
 };
 
 // Capsule wrapper over localStorage native getItem method
 capsule.prototype.getItem = function(id) {
-	var self = this;
+    var self = this;
 
-	return self._storage.getItem(id);
+    return self._storage.getItem(id);
 };
 
 // Capsule wrapper over localStorage native removeItem method
 capsule.prototype.removeItem = function(id) {
-	var self = this
-	,	dataObj = void 0;
+    var self = this,
+        dataObj = void 0;
 
-	dataObj = self._storage.getItem(id);
-	self._storage.removeItem(id);
+    dataObj = self._storage.getItem(id);
+    self._storage.removeItem(id);
 
-	return dataObj;
+    return dataObj;
 }
 
 // Global capsule object
